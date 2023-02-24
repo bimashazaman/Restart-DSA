fff310e29312d0618572e4e8dfd3a846e0f64a7e
bimashazaman <bimashazaman1234.com>
2023-02-24 20:55:03 +0600

    Initial commits

--- Structures.cpp
+++ Structures.cpp
@@ -16,6 +16,15 @@ int main() {
     /* Creating a variable of type struct Rec. */
     struct Rec r1 = {10,5};
 
-    printf("%lu", sizeof(r1)); //8 bytes
+   /* Re-Assigning values to the variables of the struct. */
+    r1.len = 100;
+    r1.bredth = 200;
+
+  /* Printing the value of len and bredth of the struct r1. */
+    cout<<r1.len<<endl;
+    cout<<r1.bredth<<endl;
+    
+
+    // printf("%lu", sizeof(r1)); 
     return 0;
 };
