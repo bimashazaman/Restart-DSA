6e119c1f64ce4972e5a0e5b3b7916fb26d9bb7e5
bimashazaman <bimashazaman1234.com>
2023-02-25 00:11:57 +0600

    Heap array

---
+++ ArrayInHeap.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std; 
+
+
+
+int main() {
+   /* Allocating memory for 5 integers. */
+    int *p;
+    p = (int *)malloc(5*sizeof(int));
+
+    p[1] = 10;
+    p[2] = 1430;
+    p[3] = 120;
+    p[4] = 130;
+    p[5] = 120;
+
+/* Printing the values of the array. */
+     for(int i= 0; i<5; i++)
+        cout<<p[i]<<endl;
+}
