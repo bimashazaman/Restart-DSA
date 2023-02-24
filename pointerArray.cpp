6633794e88c6cfe1a67aa0daec7244be7201f191
bimashazaman <bimashazaman1234.com>
2023-02-24 21:22:30 +0600

    Pointer array

---
+++ pointerArray.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std; 
+
+int main () {
+
+    int A[5] = {1,34,45,56,4};
+    int *p;
+
+    p=A; // dont need to use & for getting the address of A[5] because A is an address of the emems in the array
+
+    for(int i= 0; i<5; i++)
+        cout<<p[i]<<endl;
+
+    return 0;
+};
--- pointers.cpp
+++ pointers.cpp
@@ -4,8 +4,6 @@
 
 using namespace std; 
 
-
-
 int main () {
 
     int a = 10; // consumes 4 bytes
