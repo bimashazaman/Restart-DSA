2579e640794d97864c571b1d696a75831fccf9f7
bimashazaman <bimashazaman1234.com>
2023-02-26 03:43:44 +0600

    Templating

---
+++ Templating.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+template <class T>
+class Arithmetic
+{
+private:
+    T a;
+    T b;
+
+public:
+    Arithmetic(T a, T b);
+    T add();
+};
+
+template <class T>
+Arithmetic<T>::Arithmetic(T a, T b)
+{
+    this->a = a;
+    this->b = b;
+};
+
+template <class T>
+T Arithmetic<T>::add()
+{
+    return a + b;
+}
+
+int main()
+{
+
+    Arithmetic<int> ar(10, 45);
+
+    cout << ar.add();
+
+    return 0;
+}
