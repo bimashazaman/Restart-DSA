f85bd0198ecd6c48b9f6bec66eae39d87337dbb6
bimashazaman <bimashazaman1234.com>
2023-02-27 02:11:30 +0600

    Recursion

---
+++ Recursion.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void fun(int n)
+{
+    if (n > 0)
+    {
+        cout << n << endl;
+        fun(n - 1);
+    }
+}
+
+int main()
+{
+    int x = 4;
+
+    fun(x);
+    return 0;
+}
