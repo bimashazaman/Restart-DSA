e442dd16dddb38aacb5d7f6b09ea6dec8f871375
bimashazaman <bimashazaman1234.com>
2023-02-28 01:08:56 +0600

    Fibonocci

---
+++ Fibonocci.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int fib(int n)
+{
+    int i = 0, t0 = 0, t1 = 1, s = 0;
+
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    for (i = 2; i <= n; i++)
+    {
+        s = t0 + t1;
+        t0 = t1;
+        t1 = s;
+    }
+
+    return s;
+}
+
+int rfib(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    return rfib(n - 2) + rfib(n - 1);
+}
+
+int main()
+{
+
+    cout << rfib(10);
+    return 0;
+}
