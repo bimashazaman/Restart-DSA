60612fa1fc5cabacf784afe7c51c3a923893ef7c
bimashazaman <bimashazaman1234.com>
2023-02-27 21:38:23 +0600

    Sum of N using recursion

---
+++ sumOfNinRecursion.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum(n - 1) + n;
+    }
+}
+int loopSum(int n)
+{
+    int s = 0, i;
+    /* Adding all the numbers from 0 to n. */
+    for (i = 0; i <= n; i++)
+    {
+        s = s + i;
+    }
+    return s;
+}
+
+int main()
+{
+    int r = 5;
+
+    int result = loopSum(r);
+
+    cout << result;
+    return 0;
+}
