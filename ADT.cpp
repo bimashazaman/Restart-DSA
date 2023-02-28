3d8832ae0dbc879069366c753d9df6858ef9000a
bimashazaman <bimashazaman1234@gmail.com>
2023-03-01 03:46:55 +0600

    Initial commit

---
+++ ADT.cpp
@@ -0,0 +1,93 @@
+
+#include <stdio.h>
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+int deleteAtGivenIndex(struct Array *arr, int givenIndex)
+{
+    int x = 0, i;
+    if (givenIndex >= 0 && givenIndex < arr->length)
+    {
+        x = arr->A[givenIndex];
+        for (i = givenIndex; i < arr->length - 1; i++)
+        {
+            arr->A[i] = arr->A[i + 1];
+        }
+
+        arr->length--;
+        return x;
+    }
+
+    return 0;
+}
+
+int linearSearch(struct Array arr, int key)
+{
+    int i;
+
+    for (i = 0; i < arr.length; i++)
+    {
+        if (key = arr.A[i])
+        {
+            return i;
+        }
+
+        return -1;
+    }
+}
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\nElements are\n");
+    for (i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+}
+void Append(struct Array *arr, int x)
+{
+    if (arr->length < arr->size)
+        arr->A[arr->length++] = x;
+}
+void Insert(struct Array *arr, int index, int x)
+{
+    int i;
+
+    if (index >= 0 && index <= arr->length)
+    {
+        for (i = arr->length; i > index; i--)
+            arr->A[i] = arr->A[i - 1];
+        arr->A[index] = x;
+        arr->length++;
+    }
+}
+
+void insertPracticeOnceAgain(struct Array *arr, int index, int x)
+{
+    int i;
+
+    if (index >= 0 && index <= arr->length)
+    {
+        /* Shifting the elements to the right. */
+        for (i = arr->length; i > index; i--)
+        {
+            /* Shifting the elements to the right. */
+            arr->A[i] = arr->A[i - 1];
+        }
+        /* Assigning the value of x to the index of the array. */
+        arr->A[index] = x;
+        /* Incrementing the length of the array. */
+        arr->length++;
+    }
+}
+int main()
+{
+    struct Array arr1 = {{2, 3, 4, 5, 6}, 10, 5};
+    Append(&arr1, 10);
+    Insert(&arr1, 0, 12);
+    Display(arr1);
+    return 0;
+}
