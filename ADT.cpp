fdd74876f22480f452a6b39d4d3693da8d06c15a
bimashazaman <bimashazaman1234@gmail.com>
2023-03-01 05:46:42 +0600

    Max

--- ADT.cpp
+++ ADT.cpp
@@ -44,6 +44,23 @@ int Get(struct Array arr, int index)
     return -1;
 }
 
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    int i;
+
+    /* Returning the max value of the array. */
+    for (i = 1; i < arr.length; i++)
+    {
+        if (arr.A[i] > max)
+        {
+            return max = arr.A[i];
+        }
+
+        return max;
+    }
+}
+
 int Set(struct Array *arr, int index, int x)
 {
     if (index >= 0 && index < arr->length)
