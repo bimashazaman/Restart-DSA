be7e92d18f65d4770af418a38570e408830cb8fc
bimashazaman <bimashazaman1234@gmail.com>
2023-03-02 03:06:33 +0600

    Initial commit

--- ADT.cpp
+++ ADT.cpp
@@ -7,6 +7,69 @@ struct Array
     int length;
 };
 
+void InsertSort(struct Array *arr, int x)
+{
+    int i = arr->length - 1;
+    if (arr->length == arr->size)
+    {
+        return;
+    }
+
+    while (i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr)
+{
+    int i;
+
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void Rearrange(struct Array *arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+
+    while (i < j)
+    {
+        while (arr->A[i] < 0)
+        {
+            i++;
+        }
+        while (arr->A[j] >= 0)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            swap(&arr->A[i], &arr->A[j]);
+        }
+    }
+}
+
 void Reverse(struct Array *arr)
 {
     int *B;
