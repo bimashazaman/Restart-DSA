ed4611c4e1584af59a1133b887250355a7de7dbc
bimashazaman <bimashazaman1234@gmail.com>
2023-03-04 05:48:19 +0600

    Linked list

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -51,6 +51,115 @@ void Rdisplay(struct Node *p)
     }
 }
 
+int count(struct Node *p)
+{
+    int l = 0;
+    while (p != NULL)
+    {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+int Rcount(struct Node *p)
+{
+    if (p != NULL)
+    {
+        return Rcount(p->next) + 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int sum(struct Node *p)
+{
+    int s = 0;
+    while (p != NULL)
+    {
+        s += p->data;
+        p = p->next;
+    }
+    return s;
+}
+
+int Rsum(struct Node *p)
+{
+    if (p != NULL)
+    {
+        return Rsum(p->next) + p->data;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int max(struct Node *p)
+{
+    int max = INT32_MIN;
+    while (p != NULL)
+    {
+        if (p->data > max)
+        {
+            max = p->data;
+        }
+        p = p->next;
+    }
+    return max;
+}
+
+int Rmax(struct Node *p)
+{
+    int x = 0;
+    if (p == 0)
+    {
+        return INT32_MIN;
+    }
+    x = Rmax(p->next);
+    if (x > p->data)
+    {
+        return x;
+    }
+    else
+    {
+        return p->data;
+    }
+}
+
+struct Node *Lsearch(struct Node *p, int key)
+{
+    struct Node *q = NULL;
+    while (p != NULL)
+    {
+        if (key == p->data)
+        {
+            q->next = p->next;
+            p->next = first;
+            first = p;
+            return p;
+        }
+        q = p;
+        p = p->next;
+    }
+    return NULL;
+}
+
+struct Node *Rsearch(struct Node *p, int key)
+{
+    if (p == NULL)
+    {
+        return NULL;
+    }
+    if (key == p->data)
+    {
+        return p;
+    }
+    return Rsearch(p->next, key);
+}
+
 int main()
 {
 
