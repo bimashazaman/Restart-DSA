bd938c6b036c95942fbc09ef73fa705991066d79
bimashazaman <bimashazaman1234@gmail.com>
2023-03-14 15:07:24 +0600

    bst

--- .vscode/BST.cpp
+++ .vscode/BST.cpp
@@ -5,15 +5,15 @@ using namespace std;
 class Node
 {
 public:
-    Node *lchild;
-    int data;
-    Node *rchild;
-};
+    Node *lchild; // left child
+    int data;     // data
+    Node *rchild; // right child
+};                // Node
 
 class BST
 {
 private:
-    Node *root;
+    Node *root; // root node
 
 public:
     BST() { root = nullptr; }
@@ -23,54 +23,54 @@ public:
     Node *Search(int key);
 };
 
-void BST::Insert(int key)
+void BST::Insert(int key) // Insert a node in BST
 {
 
-    Node *t = root;
-    Node *p;
-    Node *r;
+    Node *t = root; // t is a pointer to root
+    Node *p;        // p is a pointer to new node
+    Node *r;        // r is a pointer to parent of t
 
     // root is empty
     if (root == nullptr)
     {
-        p = new Node;
-        p->data = key;
-        p->lchild = nullptr;
-        p->rchild = nullptr;
-        root = p;
-        return;
+        p = new Node;        // create a new node
+        p->data = key;       // set data
+        p->lchild = nullptr; // set left child to null
+        p->rchild = nullptr; // set right child to null
+        root = p;            // set root to p
+        return;              // exit
     }
 
-    while (t != nullptr)
+    while (t != nullptr) // while t is not null
     {
-        r = t;
-        if (key < t->data)
+        r = t;             // set r to t
+        if (key < t->data) // if key is less than t's data
         {
-            t = t->lchild;
+            t = t->lchild; // set t to t's left child
         }
-        else if (key > t->data)
+        else if (key > t->data) // if key is greater than t's data
         {
-            t = t->rchild;
+            t = t->rchild; // set t to t's right child
         }
         else
         {
-            return;
+            return; // exit
         }
     }
 
     // Now t points at NULL and r points at insert location
-    p = new Node;
-    p->data = key;
-    p->lchild = nullptr;
-    p->rchild = nullptr;
+    p = new Node;        // create a new node
+    p->data = key;       // set data
+    p->lchild = nullptr; // set left child to null
+    p->rchild = nullptr; // set right child to null
 
     if (key < r->data)
     {
-        r->lchild = p;
+        r->lchild = p; // set r's left child to p
     }
     else
     {
-        r->rchild = p;
+        r->rchild = p; // set r's right child to p
     }
 }
 
