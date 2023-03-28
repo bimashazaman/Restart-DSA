0a737b40b62b69d8476fc9b625ac7625726e358f
bimashazaman <bimashazaman1234@gmail.com>
2023-03-29 01:54:28 +0600

    Insertion sort

--- Sorts.cpp
+++ Sorts.cpp
@@ -31,6 +31,25 @@ void bubbleSort(int A[], int n)
     }
 }
 
+// Insertion sort
+void insertionSort(int A[], int n)
+{
+    int i, j, x; // x is the element to be inserted
+
+    /* Sorting the array A in ascending order. */
+    for (i = 1; i < n; i++)
+    {
+        j = i - 1; // j is the index of the last element of the sorted subarray
+        x = A[i];  // x is the element to be inserted
+        while (j > -1 && A[j] > x)
+        {
+            A[j + 1] = A[j]; // shifting the elements to the right
+            j--;             // decrementing j
+        }                    // end of while loop
+        A[j + 1] = x;        // inserting x at the correct position
+    }
+}
+
 int main()
 {
     int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
